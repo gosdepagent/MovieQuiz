96b529c913bbc2f969e15c2734aee3f8b394811c
gosdepagent <velikanova6277@gmail.com>
2024-11-16 00:08:33 -0500

    Alert Update

---
+++ MovieQuiz/Modelss/AlertModel.swift
@@ -0,0 +1,12 @@
+//
+//  AlertModel.swift
+//  MovieQuiz
+//
+//  Created by Yanye Velikanova on 11/15/24.
+//
+
+import Foundation
+
+struct AlertModel {
+    
+}
---
+++ MovieQuiz/Presentation/AlertPresenterDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AlertPresenterDelegate.swift
+//  MovieQuiz
+//
+//  Created by Yanye Velikanova on 11/15/24.
+//
+
+import Foundation
